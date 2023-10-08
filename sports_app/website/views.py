a66c03729bde27eea1508d69ec25bd96ff4e31e9
Raja Nazirul <rajanazirul93@gmail.com>
2023-10-08 21:43:28 +0800

    setup basic login

--- sports_app/website/views.py
+++ sports_app/website/views.py
@@ -1,4 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def home(request):
-    return render(request, 'home.html', {})
+    # check to see if user already login
+	if request.method == 'POST':
+		username = request.POST['username']
+		password = request.POST['password']
+		# Authenticate
+		user = authenticate(request, username=username, password=password)
+		if user is not None:
+			login(request, user)
+			messages.success(request, "You Have Been Logged In!")
+			return redirect('home')
+		else:
+			messages.success(request, "There Was An Error Logging In, Please Try Again...")
+			return redirect('home')
+	else:
+		return render(request, 'home.html', {})
+
+def login_user(request):
+    pass
+
+def logout_user(request):
+	logout(request)
+	messages.success(request, "You Have Been Logged Out...")
+	return redirect('home')
+
