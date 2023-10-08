4bc058c8f95368a2266db8c7011d1ed757664068
Raja Nazirul <rajanazirul93@gmail.com>
2023-10-08 23:46:08 +0800

    add API to upload file

--- sports_app/storages/views.py
+++ sports_app/storages/views.py
@@ -1,17 +1,20 @@
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from .serializers import UploadSerializer
+from rest_framework.permissions import IsAuthenticated
 
 
 # ViewSets define the view behavior.
 class UploadViewSet(ViewSet):
     serializer_class = UploadSerializer
+    permission_classes = [IsAuthenticated]
 
     def list(self, request):
         return Response("GET API")
 
     def create(self, request):
         file_uploaded = request.FILES.get("file_uploaded")
-        content_type = file_uploaded.content_type
-        response = "POST API and you have uploaded a {} file".format(content_type)
+        # content_type = file_uploaded.content_type
+        print(file_uploaded)
+        response = "POST API and you have uploaded a {} file"
         return Response(response)
