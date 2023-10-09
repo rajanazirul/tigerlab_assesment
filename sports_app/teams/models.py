809f8b7cc607811693d7db38998306cd04cf70ae
Raja Nazirul <rajanazirul93@gmail.com>
2023-10-09 18:45:44 +0800

    add utils

--- sports_app/teams/models.py
+++ sports_app/teams/models.py
@@ -2,9 +2,8 @@ from django.db import models
 
 
 # Create your models here.
-class Teams(models.Model):
+class Team(models.Model):
     name = models.CharField(max_length=255, unique=True)
-    points = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -16,13 +15,13 @@ class Teams(models.Model):
 
 class Match(models.Model):
     home_team = models.ForeignKey(
-        "teams.teams",
+        "teams.team",
         on_delete=models.CASCADE,
         related_name="home_team_match",
     )
     home_score = models.IntegerField()
     away_team = models.ForeignKey(
-        "teams.teams",
+        "teams.team",
         on_delete=models.CASCADE,
         related_name="away_team_match",
     )
@@ -35,3 +34,13 @@ class Match(models.Model):
         verbose_name_plural = "matches"
         db_table = "match"
         unique_together = ("home_team", "away_team")
+
+
+class Ranking(models.Model):
+    team = models.OneToOneField(Team, on_delete=models.CASCADE)
+    points = models.IntegerField()
+
+    class Meta:
+        verbose_name = "ranking"
+        verbose_name_plural = "rankings"
+        db_table = "ranking"
