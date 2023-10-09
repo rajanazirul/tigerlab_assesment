65ad9e33ab50ed9dbeebcca0b9669ecbea84a50a
Raja Nazirul <rajanazirul93@gmail.com>
2023-10-09 20:08:42 +0800

    add test

--- sports_app/storages/tests.py
+++ sports_app/storages/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
 # Create your tests here.
+# should get sleep list
+
+sample_data = "Crazy Ones, 3, Rebels, 3"
+
+
+def test_process_csv_match_input():
+    response = "pass"
+    assert response == 200
