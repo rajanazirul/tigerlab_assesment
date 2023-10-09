73466d959251b72997f8f7949b2f812750bf7c15
Raja Nazirul <rajanazirul93@gmail.com>
2023-10-09 18:38:47 +0800

    make migrations

--- sports_app/sports_app/settings.py
+++ sports_app/sports_app/settings.py
@@ -53,6 +53,8 @@ INSTALLED_APPS = [
     "website",
     "rest_framework",
     "storages",
+    "teams",
+
 ]
 
 MIDDLEWARE = [
--- sports_app/teams/models.py
+++ sports_app/teams/models.py
@@ -18,13 +18,13 @@ class Match(models.Model):
     home_team = models.ForeignKey(
         "teams.teams",
         on_delete=models.CASCADE,
-        related_name="teams_match",
+        related_name="home_team_match",
     )
     home_score = models.IntegerField()
     away_team = models.ForeignKey(
         "teams.teams",
         on_delete=models.CASCADE,
-        related_name="teams_match",
+        related_name="away_team_match",
     )
     away_score = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
@@ -34,3 +34,4 @@ class Match(models.Model):
         verbose_name = "match"
         verbose_name_plural = "matches"
         db_table = "match"
+        unique_together = ("home_team", "away_team")
