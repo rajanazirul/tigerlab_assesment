4b6a464e44debbf2a2f820bc171bfce62426e4a0
Raja Nazirul <rajanazirul93@gmail.com>
2023-10-09 18:35:36 +0800

    remove rankings features

--- sports_app/teams/models.py
+++ sports_app/teams/models.py
@@ -12,3 +12,25 @@ class Teams(models.Model):
         verbose_name = "team"
         verbose_name_plural = "teams"
         db_table = "teams"
+
+
+class Match(models.Model):
+    home_team = models.ForeignKey(
+        "teams.teams",
+        on_delete=models.CASCADE,
+        related_name="teams_match",
+    )
+    home_score = models.IntegerField()
+    away_team = models.ForeignKey(
+        "teams.teams",
+        on_delete=models.CASCADE,
+        related_name="teams_match",
+    )
+    away_score = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "match"
+        verbose_name_plural = "matches"
+        db_table = "match"
