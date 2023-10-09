2f14c6a6525c291a80775dc5d8212577df14eaf2
Raja Nazirul <rajanazirul93@gmail.com>
2023-10-09 18:24:06 +0800

    create teams model

--- sports_app/teams/models.py
+++ sports_app/teams/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class Teams(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "team"
+        verbose_name_plural = "teams"
+        db_table = "teams"
