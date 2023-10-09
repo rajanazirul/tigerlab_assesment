c6823166cb3f12f0ae6fd7781f33374c8b7114b9
Raja Nazirul <rajanazirul93@gmail.com>
2023-10-09 18:31:20 +0800

    add point to the team

--- sports_app/teams/models.py
+++ sports_app/teams/models.py
@@ -4,6 +4,7 @@ from django.db import models
 # Create your models here.
 class Teams(models.Model):
     name = models.CharField(max_length=255, unique=True)
+    points = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
