b901e2e68860c369f585b1ef944a822c534d5911
Raja Nazirul <rajanazirul93@gmail.com>
2023-10-10 09:27:36 +0800

    update ranking service and test

--- sports_app/teams/tests.py
+++ sports_app/teams/tests.py
@@ -1,3 +1,39 @@
-from django.test import TestCase
+import pytest
+from teams.models import Team
+from storages.utils import FileUploadService
+from teams.utils import PointsRankingCalculator, RankingService
 
-# Create your tests here.
+
+sample_data = [
+    ["Crazy Ones, 3, Rebels, 3"],
+    ["Fantastics, 1, FC Super, 0"],
+    ["Crazy Ones, 1, FC Super, 1"],
+    ["Fantastics, 3, Rebels, 1"],
+    ["Crazy Ones, 4, Misfits, 0"],
+]
+
+
+@pytest.mark.django_db
+def test_points_ranking_calculator():
+    # create match data
+    for data in sample_data:
+        FileUploadService(data[0]).process_match_input()
+
+    team = Team.objects.get(name="Crazy Ones")
+
+    point = PointsRankingCalculator().calculate_point(team)
+
+    assert point == 5
+
+
+@pytest.mark.django_db
+def test_ranking_positioning():
+    # create match data
+    for data in sample_data:
+        FileUploadService(data[0]).process_match_input()
+
+    point = PointsRankingCalculator()
+    rankings = RankingService(points_ranking_calculator=point).calculate_rankings()
+
+    assert rankings[0].points == 6
+    assert rankings[4].points == 0
--- sports_app/teams/utils.py
+++ sports_app/teams/utils.py
@@ -1,31 +1,47 @@
-from .models import Team, Match
+from .models import Team, Match, Ranking
 from django.db.models import Q
 
 
-class RankingCalculator:
-    def calculate_ranking(self, team):
-        # Calculate the ranking for the given team based on match results
-        pass
-
-
-class PointsRankingCalculator(RankingCalculator):
-    def calculate_ranking(self, team):
-        # Calculate the ranking based on points
+class PointsRankingCalculator:
+    def calculate_point(self, team: Team):
+        """
+        Calculate the total points based on team
+        """
 
         # query match related to team
         matches = Match.objects.filter(Q(home_team=team) | Q(away_team=team))
+        total_points = 0
 
-        pass
+        # calculate point per match
+        for match in matches:
+            if match.home_team == team:
+                if match.home_score > match.away_score:
+                    total_points += 3
+                elif match.home_score == match.away_score:
+                    total_points += 1
+            else:
+                if match.away_score > match.home_score:
+                    total_points += 3
+                elif match.home_score == match.away_score:
+                    total_points += 1
+
+        # save into ranking table
+        ranking, created = Ranking.objects.get_or_create(team=team)
+        ranking.points = total_points
+        ranking.save()
+
+        return total_points
 
 
 class RankingService:
-    def __init__(self, ranking_calculator: RankingCalculator):
-        self.ranking_calculator = ranking_calculator
+    def __init__(self, points_ranking_calculator: PointsRankingCalculator):
+        self.points_ranking_calculator = points_ranking_calculator
 
     def calculate_rankings(self):
         teams = Team.objects.all()
         for team in teams:
-            ranking = self.ranking_calculator.calculate_ranking(team)
-            # Save or display the ranking as needed
+            self.points_ranking_calculator.calculate_point(team)
 
-        # ...
+        # display ranking in JSON format
+        rankings = Ranking.objects.all().order_by('-points', 'team')
+        return rankings
